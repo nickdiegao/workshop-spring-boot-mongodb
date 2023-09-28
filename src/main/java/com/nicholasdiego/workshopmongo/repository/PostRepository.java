c68e21fadc501c5bc285b9eaa7b921387820607e
Nicholas Diego <nickdiegao@gmail.com>
2023-09-28 19:55:03 -0300

    Consulta simples com query methods

--- src/main/java/com/nicholasdiego/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nicholasdiego/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.nicholasdiego.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.nicholasdiego.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
