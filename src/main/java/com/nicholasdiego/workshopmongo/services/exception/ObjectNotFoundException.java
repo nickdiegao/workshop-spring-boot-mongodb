cb718b1131328ddaa8318fad0b15da61eb1f33f1
Nicholas Diego <nickdiegao@gmail.com>
2023-09-28 16:07:45 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/nicholasdiego/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.nicholasdiego.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
